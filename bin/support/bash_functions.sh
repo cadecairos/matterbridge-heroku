0665e388bcb586f4d7a89dbc2fcb826e99d99a22
Patrick Connolly <patrick.c.connolly@gmail.com>
2018-11-21 14:38:17 +0800

    Expand PORT envvar within other envvars.

--- bin/support/bash_functions.sh
+++ bin/support/bash_functions.sh
@@ -18,8 +18,11 @@ export_env_dir() {
   blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
+      PORT="$(cat $env_dir/PORT)"
       echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
-      export "$e=$(cat $env_dir/$e)"
+        port_unexpanded=$(echo "$e=$(cat $env_dir/$e)") &&
+        port_expanded=$(eval "echo $port_unexpanded") &&
+        export $port_expanded
       :
     done
   fi
